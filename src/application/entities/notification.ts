29edfa53571f8e1dd7aa9aaea62f957009c118c3
johnjoker13 <joaoavelino2008@hotmail.com>
2022-12-14 18:25:21 -0300

    feat(notification.ts): creates notification entity

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,57 @@
+import { Replace } from '../../helpers/Replace';
+import { Content } from './content';
+
+export interface NotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readtAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private _props: NotificationProps;
+
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public set recipientId(recipientId: string) {
+    this._props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this._props.recipientId;
+  }
+
+  public set content(content: Content) {
+    this._props.content = content;
+  }
+
+  public get content(): Content {
+    return this._props.content;
+  }
+
+  public set category(category: string) {
+    this._props.category = category;
+  }
+
+  public get category(): string {
+    return this._props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this._props.readtAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this._props.readtAt;
+  }
+
+  public get createdAt(): Date {
+    return this._props.createdAt;
+  }
+}
