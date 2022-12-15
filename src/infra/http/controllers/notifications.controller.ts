c81375e14c75d8f2f175403c95e0c8437543784f
johnjoker13 <joaoavelino2008@hotmail.com>
2022-12-15 13:20:24 -0300

    refactor(notifications.controller.ts): renames app.controller to notifications.controller

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { SendNotification } from '../../../application/use-cases/send-notification';
+import { CreateNotificationBody } from '../dtos/create-notification-body';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private _sendNotification: SendNotification) {}
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    const { notification } = await this._sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return { notification };
+  }
+}
