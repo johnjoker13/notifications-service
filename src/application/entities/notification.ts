fee14c49cde599decb15d7cd00324ef6073940a4
johnjoker13 <joaoavelino2008@hotmail.com>
2022-12-15 16:50:36 -0300

    feat(notifications.ts): creates new property canceledAt and its getter and setter

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -7,6 +7,7 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readtAt?: Date | null;
+  canceledtAt?: Date | null;
   createdAt: Date;
 }
 
@@ -56,6 +57,14 @@ export class Notification {
 
   public get readAt(): Date | null | undefined {
     return this._props.readtAt;
+  }
+
+  public cancel() {
+    this._props.canceledtAt = new Date();
+  }
+
+  public get canceledAt(): Date | null | undefined {
+    return this._props.canceledtAt;
   }
 
   public get createdAt(): Date {
