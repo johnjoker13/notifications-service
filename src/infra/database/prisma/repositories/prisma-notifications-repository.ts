01eaab4c8527be6e265390892244594b0af63c6d
johnjoker13 <joaoavelino2008@hotmail.com>
2022-12-15 16:53:59 -0300

    feat(prisma-notification-repository/in-memory-notification-repository): adds save and findById

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -7,10 +7,18 @@ import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper'
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private _prismaService: PrismaService) {}
+
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
     await this._prismaService.notification.create({
       data: raw,
     });
+  }
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
