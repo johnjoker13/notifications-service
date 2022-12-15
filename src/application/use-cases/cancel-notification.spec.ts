53a199da732b059f6e59999b335a46355450a9b9
johnjoker13 <joaoavelino2008@hotmail.com>
2022-12-15 16:54:45 -0300

    test(cancel-notification.spec.ts): tests cancel a notification

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,39 @@
+import { Content } from '../entities/content';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { Notification } from '../entities/notification';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './erros/notification-not-found';
+
+describe('Cancel notification', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = new Notification({
+      category: 'social',
+      content: new Content('New friendship solicitation!'),
+      recipientId: 'example-recipient-id',
+    });
+
+    notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('it should not be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
