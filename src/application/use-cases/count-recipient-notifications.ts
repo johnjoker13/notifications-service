cf5c0cd5bbb8b5975b6f5ba4a67eaa90bd946ae3
johnjoker13 <joaoavelino2008@hotmail.com>
2022-12-15 21:40:48 -0300

    feat(count-recipient-notifications.ts): implements CountRecipientNotifications

---
+++ src/application/use-cases/count-recipient-notifications.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface CountRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface CountRecipientNotificationsResponse {
+  count: number;
+}
+
+@Injectable()
+export class CountRecipientNotifications {
+  constructor(private _NotificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: CountRecipientNotificationsRequest,
+  ): Promise<CountRecipientNotificationsResponse> {
+    const { recipientId } = request;
+
+    const count = await this._NotificationsRepository.countManyByRecipientId(
+      recipientId,
+    );
+
+    return {
+      count,
+    };
+  }
+}
