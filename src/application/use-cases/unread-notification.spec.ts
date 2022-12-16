ef12e4c703144b58a659e3aaafb725fa3c5a4845
johnjoker13 <joaoavelino2008@hotmail.com>
2022-12-15 21:42:09 -0300

    test(unread-notification.spec.ts): tests UnreadNotification

---
+++ src/application/use-cases/unread-notification.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { NotificationNotFound } from './erros/notification-not-found';
+import { makeNotification } from '../../../test/factories/notification-factory';
+import { UnreadNotification } from './unread-notification';
+
+describe('Unread notification', () => {
+  it('should be able to unread a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    const notification = makeNotification({
+      readtAt: new Date(),
+    });
+
+    notificationsRepository.create(notification);
+
+    await unreadNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].readAt).toBeNull();
+  });
+
+  it('it should not be able to unread an unexistent notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new UnreadNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
