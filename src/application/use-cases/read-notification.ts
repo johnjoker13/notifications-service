731f2083756a27ed937a48619b8cae8d3bace2e5
johnjoker13 <joaoavelino2008@hotmail.com>
2022-12-15 21:41:43 -0300

    feat(red-notification.ts): implements readNotification

---
+++ src/application/use-cases/read-notification.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+import { NotificationNotFound } from './erros/notification-not-found';
+
+interface ReadNotificationRequest {
+  notificationId: string;
+}
+
+type ReadNotificationResponse = void;
+
+@Injectable()
+export class ReadNotification {
+  constructor(private _NotificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: ReadNotificationRequest,
+  ): Promise<ReadNotificationResponse> {
+    const { notificationId } = request;
+
+    const notification = await this._NotificationsRepository.findById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+
+    notification.read();
+
+    await this._NotificationsRepository.save(notification);
+  }
+}
