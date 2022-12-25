187623e19341e26c5d105846a679eb0a7fb85d8c
johnjoker13 <joaoavelino2008@hotmail.com>
2022-12-25 11:18:15 -0300

    build(package*.json): installs rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { MicroserviceOptions } from '@nestjs/microservices/interfaces';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  // app.useGlobalPipes(new ValidationPipe());
+  // await app.listen(3000);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          'amqps://uaiqttac:sIqevVUNDaN2DmO_ozBOxkFPMHsEiJKe@jackal.rmq.cloudamqp.com/uaiqttac',
+        ],
+        queue: 'notification_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  app.listen();
 }
+
 bootstrap();
